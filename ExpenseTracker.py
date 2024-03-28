e75f1479aa010f9994449ee7f4d2aa0d2bcef83c
Arif <arifulislam@AIs-Macbook.local>
2024-03-28 18:11:15 +1100

    Second attemp changed by Zorigo and Arif

--- ExpenseTracker.py
+++ ExpenseTracker.py
@@ -28,7 +28,7 @@ class ExpenseTracker:
 def main():
     tracker = ExpenseTracker()
 
-    print("Welcome to Daily Expense Tracker App")
+    print("Welcome to Daily Expense Tracker App feature-y!")
 
     while True:
         print("\nSelect an option:")
